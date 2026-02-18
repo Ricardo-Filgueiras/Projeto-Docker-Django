25281f746f4089401b63d86b84155bc6a7d47bf0
Ricardo filgueiras <ricardofilgueiras70@gmail.com>
2026-02-18 16:23:07 -0300

    Ambiente django no Docker

---
+++ scripts/wait_psql.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 2
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic --noinput --clear
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
