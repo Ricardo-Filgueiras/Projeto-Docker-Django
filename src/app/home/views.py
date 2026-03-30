100d43ec852a445d4b19fef54f4ae3c0ad67de73
Ricardo Filgueiras <107813151+Ricardo-Filgueiras@users.noreply.github.com>
2026-03-30 10:05:00 -0300

    update app home

--- src/app/home/views.py
+++ src/app/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('home/', views.index, name='index'),
+# ] 
+
